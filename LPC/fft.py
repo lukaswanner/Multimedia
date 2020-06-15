9453fba5edc4d8368d9c5a9f4da5c2b6fba40a76
Lukas <lukas.wanner@web.de>
2020-06-15 17:10:57 +0200

    started with exercise 3

--- LPC/fft.py
+++ LPC/fft.py
@@ -16,21 +16,24 @@ def fft(inputSignal, nDeleteFreq, fftLength):
     outputSignal = 0
     arr = numpy.fft.fft(inputSignal[1])
     arrabs = numpy.abs(arr)
-    fig, axs = plt.subplots(2)
-    fig.suptitle('Vertically stacked subplots')
-    axs[0].plot(arr)
+    #fig, axs = plt.subplots(2)
+    #fig.suptitle('Vertically stacked subplots')
+    #axs[0].plot(arr)
     sortedarr = numpy.sort(arrabs)
     for i in range(nDeleteFreq):
         index = numpy.where(arrabs == sortedarr[i])
         arr[index] = 0
-    axs[1].plot(arr)
+    #axs[1].plot(arr)
+    #plt.show()
+    outputSignal = numpy.fft.ifft(arr)
+    axs[1].plot(outputSignal)
     plt.show()
     return outputSignal
 
 def main(pieces):
     # output = fft()
-    for i in range(len(pieces)):
-        output = fft(pieces[i],80000,len(pieces[i][1]))
+    output = fft(pieces[0],80000,len(pieces[0][1]))
+    scipy.io.wavfile.write("out.wav",16000,output)
     print("")
 
 
@@ -42,4 +45,7 @@ if __name__ == "__main__":
     while i != len(data[1]):
         pieces.append(data[i:i + length])
         i += length
+    fig, axs = plt.subplots(2)
+    fig.suptitle('Vertically stacked subplots')
+    axs[0].plot(pieces[0][1])
     main(pieces)
