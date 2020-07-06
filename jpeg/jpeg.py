5b3b7ad739271f1bd45582792b66085089a9ba93
Lukas <lukas.wanner@web.de>
2020-07-06 17:23:56 +0200

    updated the function to handle blocks

--- jpeg/jpeg.py
+++ jpeg/jpeg.py
@@ -1,16 +1,17 @@
 import numpy as np
 from PIL import Image
+from scipy import fftpack as fft
+
 
 # This function demonstrates the jpeg encoding
 #
 #   input  :   original image
 #   output :   matrix with quantised DCT values
 def jpegEncode(input):
-
     # DCT matrix
     ar = np.array([range(8)])
-    T = np.array(0.5 * np.cos(ar.T * (2*ar+1) * np.pi / 16))
-    T[0,:] = np.sqrt(1/8)
+    T = np.array(0.5 * np.cos(ar.T * (2 * ar + 1) * np.pi / 16))
+    T[0, :] = np.sqrt(1 / 8)
 
     im = np.array(Image.open(input))
     pixellen = len(im)
@@ -21,33 +22,31 @@ def jpegEncode(input):
 
     for row, val in enumerate(imgsplit):
         for col in range(numsub):
-            #print(row, col, ":")
-            #print(val[:, col*8:(col+1)*8])
-            imglist.append(val[:, col*8:(col+1)*8])
-
+            # print(row, col, ":")
+            # print(val[:, col*8:(col+1)*8])
+            imglist.append(val[:, col * 8:(col + 1) * 8])
 
     dctlist = []
 
     for matrix in imglist:
-        dctlist.append(np.dot(np.dot(T,matrix),np.transpose(T)))
+        dctlist.append(np.dot(np.dot(T, matrix), np.transpose(T)))
 
     # Luminance quantization matrix
-    q = [   [16, 11, 10, 16, 24, 40, 51, 61],
-            [12, 12, 14, 19, 26, 58, 60, 55],
-            [14, 13, 16, 24, 40, 57, 69, 56],
-            [14, 17, 22, 29, 51, 87, 80, 62],
-            [18, 22, 37, 56, 68, 109, 103, 77],
-            [24, 35, 55, 64, 81, 104, 113, 92],
-            [49, 64, 78, 87, 103, 121, 120, 101],
-            [72, 92, 95, 98, 112, 100, 103, 99]]
+    q = [[16, 11, 10, 16, 24, 40, 51, 61],
+         [12, 12, 14, 19, 26, 58, 60, 55],
+         [14, 13, 16, 24, 40, 57, 69, 56],
+         [14, 17, 22, 29, 51, 87, 80, 62],
+         [18, 22, 37, 56, 68, 109, 103, 77],
+         [24, 35, 55, 64, 81, 104, 113, 92],
+         [49, 64, 78, 87, 103, 121, 120, 101],
+         [72, 92, 95, 98, 112, 100, 103, 99]]
 
     output = []
     for matrix in dctlist:
-        output.append(np.divide(matrix,q))
-
-    output = np.around(output,0)
+        output.append(np.divide(matrix, q))
 
     return output
+
 
 # This function demonstrates the jpeg decoding
 #
@@ -57,7 +56,7 @@ def jpegDecode(input):
     # DCT matrix
     ar = np.array([range(8)])
     T = np.array(0.5 * np.cos(ar.T * (2 * ar + 1) * np.pi / 16))
-    T = np.linalg.inv(T)
+    T[0, :] = np.sqrt(1 / 8)
     # Luminance quantization matrix
     q = [[16, 11, 10, 16, 24, 40, 51, 61],
          [12, 12, 14, 19, 26, 58, 60, 55],
@@ -70,21 +69,49 @@ def jpegDecode(input):
 
     dctlist = []
     for matrix in input:
-        dctlist.append(np.multiply(matrix,q))
+        dctlist.append(np.multiply(matrix, q))
 
     output = []
     for matrix in dctlist:
-        output.append(np.dot(np.dot(T,matrix),np.transpose(T)))
+        output.append(np.dot(np.dot(np.transpose(T), matrix), T))
 
-    print(output[0][0])
+    output = np.round(output, 0)
+
+    output = output.astype(int)
+
+
+    i = 0
+    switch = False
+    newline = None
+    xaxis = []
+    for matrix in output:
+        if newline is None:
+            newline = matrix
+        else:
+            i += 1
+            newline = np.concatenate((newline, matrix), axis=1)
+            if i % (np.sqrt(len(output))-1) == 0:
+                xaxis.append(newline)
+                print(len(newline[0]))
+                newline = None
+
+    finished = np.concatenate(xaxis, axis=0)
+
+    img = Image.fromarray(finished)
+    img.save('my.png')
+    img.show()
+
+    print(finished.shape)
 
     return output
+
 
 def main():
     input = "house.jpg"
 
     encoded = jpegEncode(input)
     decoded = jpegDecode(encoded)
+
 
 if __name__ == "__main__":
     main()
